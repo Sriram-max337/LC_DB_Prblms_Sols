38e5a2de81f67488f2977c6f9a81bc2b54732175
Sriram-max337 <rohitvsriram@gmail.com>
2026-08-24 19:07:16 +0530

    LC 1587 done

---
+++ 1587. Bank Account Summary II.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+SELECT Users.name, SUM(Transactions.amount) AS balance FROM Users
+JOIN Transactions ON Users.account = Transactions.account
+GROUP BY Users.name
+HAVING SUM(Transactions.amount) > 10000
